03e530b49a887b8e26e879d377874a73b1a94a83
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-19 15:35:53 -0400

    Javascript Initials

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.get ElementById('submit');
+
+const myQuestions = [
+    {
+        question #1: "During which period of the Mesozoic Era did the Tyrannosaurus-Rex live?",
+        answers: {
+            a: Cretaceous
+            b: Jurassic
+            c: Triassic
+            d: None of the Above
+        },
+        correctAnswer: "a"
+    },
+]
+
+function buildQuiz() {}
+
+function showResults() {}
+
+buildQuiz;
+
+submitButton.addEventListener('click', showResults);
