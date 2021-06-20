9a2f1a958329c295758dd71aed589cfb2f8502ac
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-20 15:25:32 -0400

    Answers and output

--- assets/script.js
+++ assets/script.js
@@ -53,11 +53,36 @@ const myQuestions = [
             },
             correctAnswer: "d"
     }
+];
 
+function buildQuiz() {
+    const output = [];
 
-]
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+        
+            const answers = [];
 
-function buildQuiz() {}
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    <label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answer[letter]}
+                        </label>
+                );   
+            }
+            output.push (
+                <div class="question"> ${currentQuestion.question}</div>
+                <div class="answers"> ${answers.join('')}</div>
+            );
+        }
+    );
+
+        }
+
+    )
+}
 
 function showResults() {}
 
