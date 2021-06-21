a45e0f8a10a904f5e962ea14a9576e20056b0409
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-21 15:04:21 -0400

    More Debugging

--- assets/script.js
+++ assets/script.js
@@ -14,14 +14,16 @@ function buildQuiz() {
                         <input type="radio" name="question${questionNumber}" value="${letter}">
                         ${letter} :
 
-                        ${currentQuestion.answer[letter]}
+                        ${currentQuestion.answers[letter]}
                         </label>`
                 );   
             }
 
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
+                `<div class = "slide>
+                <div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>
+                </div>`
             );
         }
     );
