d8cee8d44adfad597c6758df7f84098813b94029
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-22 18:12:22 -0400

    Timer function

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,30 @@ var score = 0;
 function startGame() {
   isWin = false;
   timerCount = 10;
+  startButton.disabled = true;
+  renderBlanks()
+  startTimer()
+}
+function startTimer() {
+
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+     
+      if (isWin && timerCount > 0) {
+      
+        clearInterval(timer);
+        winGame();
+      }
+    }
+ 
+    if (timerCount === 0) {
+   
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
 }
 
 (function(){
