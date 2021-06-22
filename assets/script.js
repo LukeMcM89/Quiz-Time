ab94e6f99dd68b80099018cc184ab94e509bba0f
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-22 15:29:04 -0400

    Updates

--- assets/script.js
+++ assets/script.js
@@ -1,166 +1,197 @@
-(function () {
+(function(){
+  // Functions
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
 
-    function buildQuiz() {
-        const output = [];
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-        myQuestions.forEach(
-            (currentQuestion, questionNumber) => {
+        // variable to store the list of possible answers
+        const answers = [];
 
-                const answers = [];
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
 
-                for (letter in currentQuestion.answers) {
-                    answers.push(
-                        `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} :
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
 
-                        ${currentQuestion.answers[letter]}
-                        </label>`
-                    );
-                }
-
-                output.push(
-                    `<div class="slide">
-                <div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>
-                </div>`
-                );
-            }
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
         );
-        quizContainer.innerHTML = output.join('');
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers blue
+        answerContainers[questionNumber].style.color = 'blue';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
     }
-
-    function showResults() {
-
-        const answerContainers = quizContainer.querySelectorAll('.answers');
-
-        let numCorrect = 0;
-
-        myQuestions.forEach((currentQuestion, questionNumber) => {
-
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-            if (userAnswer === currentQuestion.correctAnswer) {
-
-                numCorrect++;
-
-                answerContainers[questionNumber].style.color = 'lightblue';
-            }
-
-            else {
-                answerContainers[questionNumber].style.color = 'red';
-            }
-        });
-
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    else{
+      previousButton.style.display = 'inline-block';
     }
-
-    function showSlide(n) {
-
-        slides[currentSlide].classList.remove("active-slide");
-
-        slides[n].classList.add("active-slide");
-
-        currentSlide = n;
-
-        if (currentSlide === 0) {
-            previousButton.style.display = "none";
-        }
-
-        else {
-            previousButton.style.display = "inline-block";
-        }
-
-        if (currentSlide === slides.length - 1) {
-            nextButton.style.display = "none";
-            submitButton.style.display = "inline-block";
-        }
-
-        else {
-            nextButton.style.display = "inline-block";
-            submitButton.style.display = "none";
-        }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
     }
-
-    function showNextSlide() {
-        showSlide(currentSlide + 1);
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
     }
+  }
 
-    function showPreviousSlide() {
-        showSlide(currentSlide - 1);
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Variables
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "During which period of the Mesozoic Era did the Tyrannosaurus rex live?",
+      answers: {
+        a: "Cretaceous",
+        b: "Jurassic",
+        c: "Triassic",
+        d: "None of the above"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "More than a 100 years ago, a paleontologist from the American Museum of Natural History discovered the first fossils of Tyrannosaurus rex. Who found them and where?",
+      answers: {
+        a: "Luke McMurtrie in Richmond, Virginia",
+        b: "Barnum Brown in Hell Creek, Montana, U.S.A",
+        c: "Dr. Ian Malcolm along the coast of the Osa Peninsula, Costa Rica",
+        d: "Dr. John Hammon in the Gobi Desert, Northern China"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "T.Rex grew much bigger and faster than humans. How many pounds (kilograms) do scientists maintain they gained per DAY?",
+      answers: {
+        a: "1 pound (0.5 kilograms)",
+        b: "4.6 pounds (2.1 kilograms)",
+        c: "2 pounds (1 kilogram)",
+        d: "13 pounds (5.9 kilograms)"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What color do paleontologists, scientists and scholars believe the Tyrannosaurus was?",
+      answers: {
+        a: "We don't know",
+        b: "Greyish-Blue",
+        c: "Blood-Red",
+        d: "Brown"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "At a goliath 40 feet long and 13 feet tall at the hip, Sue, the largest Tyrannosaurus rex specimen ever discovered, is currently on display at which museum?",
+      answers: {
+        a: "Black Hills Institute of Geological Research",
+        b: "California Museum of Natural History",
+        c: "The Vatican Museum",
+        d: "Field Museum of Natural History in Chicago"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Tyrannosaurus rex, derived from Latin and Greek means what?",
+      answers: {
+        a: "Big-Boi",
+        b: "Sharp-toothed reptile",
+        c: "Tyrant lizard",
+        d: "Two-clawed terror"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "BONUS: In the movie Jurassic Park, Dr. Ian Malcolm says the famous line: Life ____?",
+      answers: {
+        a: "Is stupid and contagious",
+        b: "Generally sucks",
+        c: "Is the persistent illusion of time within a quantum paradox",
+        d: "Uh...finds a way"
+      },
+      correctAnswer: "d"
     }
+  ];
 
+  // Kick things off
+  buildQuiz();
 
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-        {
-            question: "During which period of the Mesozoic Era did the Tyrannosaurus rex live?",
-            answers: {
-                a: "Cretaceous",
-                b: "Jurassic",
-                c: "Triassic",
-                d: "None of the Above"
-            },
-            correctAnswer: "a"
-        },
-        {
-            question: "More than a hundred years ago, a paleontologist from the American Museum of Natural History discovered the first fossils of <em>Tyrannosaurus rex<em>. Who found the fossils and where?",
-            answers: {
-                a: "Luke McMurtrie in Richmond, Virginia",
-                b: "Barnum Brown in Hell Creek, Montana, U.S.A.",
-                c: "Dr. Ian Malcolm along the coast of the Osa Peninsula, Costa Rica",
-                d: "Dr. John Hammond in the Gobi Desert, Northern China"
-            },
-            correctAnswer: "b"
-        },
-        {
-            question: "T.Rex grew much bigger and much faster than humans. How many pounds (kilograms) do scientists maintain they gained per DAY?",
-            answers: {
-                a: "1 pound (0.5 kilograms)",
-                b: "4.6 pounds (2.1 kilograms",
-                c: "2 pounds (1 kilogram)",
-                d: "13 pounds (5.9 kilograms)"
-            },
-            correctAnswer: "b"
-        },
-        {
-            question: "What color do paleontologists, scientists and scholars believe the Tyrannosaurus rex was?",
-            answers: {
-                a: "We don't know",
-                b: "Greyish-Blue",
-                c: "Blood-Red",
-                d: "Brown"
-            },
-            correctAnswer: "a"
-        },
-        {
-            question: "At a goliath 40 feet long and 13 feet tall at the hip, Sue, the largest Tyrannosaurus rex specimen ever discovered, is currently on display at which museum?",
-            answers: {
-                a: "Black Hills Institute of Geological Research",
-                b: "California Museum of Natural History",
-                c: "The Vatican Museum",
-                d: "Field Museum of Natural History in Chicago"
-            },
-            correctAnswer: "d"
-        }
-    ];
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
 
-    buildQuiz();
+  // Show the first slide
+  showSlide(currentSlide);
 
-    const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-
-    showSlide(currentSlide);
-
-    submitButton.addEventListener('click', showResults);
-
-    previousButton.addEventListener("click", showPreviousSlide);
-
-    nextButton.addEventListener("click", showNextSlide);
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
 })();
